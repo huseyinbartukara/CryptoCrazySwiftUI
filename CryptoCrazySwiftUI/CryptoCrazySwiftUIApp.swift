19c367cca9257c9149e40a53d4ce7ae5071eca04
Hüseyin Bartu kara <huseyinbartukara@gmail.com>
2023-07-27 13:53:05 +0300

    Initial Commit

---
+++ CryptoCrazySwiftUI/CryptoCrazySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCrazySwiftUIApp.swift
+//  CryptoCrazySwiftUI
+//
+//  Created by Bartu Kara on 27.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCrazySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
